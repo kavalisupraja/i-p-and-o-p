a3a7db49afac083b023833218f616d0566c831c0
kavalisupraja <ksupraja176@gmail.com>
2024-09-30 11:09:41 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Arthimetic
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the value of a:");
+int a=sc.nextInt();
+System.out.println("enter the value of b:");
+int b=sc.nextInt();
+sum=a+b;
+System.out.println("sum is:"+sum);
+sub=a-b;
+System.out.println("subtraction is:"+sub);
+mul=a*b;
+System.out.println("multiplication is:"+mul);
+}
+}
